11b85108eeffdd8498bd5290f6b5843f6b366c86
okadirkartal <161307071@kocaeli.edu.tr>
2023-08-13 02:34:15 +0400

    common library added

---
+++ Common/IEntity.cs
@@ -0,0 +1,6 @@
+namespace Common;
+
+public interface IEntity
+{
+    Guid Id { get; set; }
+}
---
+++ Common/Settings/MongoDbSettings.cs
@@ -0,0 +1,10 @@
+namespace Common.Settings;
+
+public class MongoDbSettings
+{
+    public string Host { get; set; }
+
+    public int Port { get; set; }
+
+    public string ConnectionString => $"mongodb://{Host}:{Port}";
+}
